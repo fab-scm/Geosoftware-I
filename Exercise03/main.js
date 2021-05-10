fce9e961575e2c70c56cac94e622d875f207e434
fab-scm <fabian.schumacher@uni-muenster.de>
2021-05-10 18:35:16 +0200

    uncommented version

--- Exercise03/main.js
+++ Exercise03/main.js
@@ -1,15 +1,15 @@
-function success(pos) {
-    var position = pos.coords;
+"use strict"
 
-    document.getElementById("coordinates").innerHTML = `Coordinates (lat/lng): ( ${position.latitude}, ${position.longitude} )`;
-    document.getElementById("accuracy").innerHTML = `Accuracy:  ${position.accuracy}`;
-
-    loadWeatherAtPosition(position.latitude, position.longitude);
-}
+let buttonPosition = document.getElementById("buttonPosition");
+let currentWeather = document.getElementById("currentWeather");
+let currentPosition = document.getElementById("currentPosition")
 
 
-
-
+/**
+ * The function loads the weather at the given position
+ * @param {*} latitude 
+ * @param {*} longitude 
+ */
 function loadWeatherAtPosition(latitude, longitude) {
     var xhttp = new XMLHttpRequest()
     xhttp.onreadystatechange = function() {
@@ -24,35 +24,79 @@ function loadWeatherAtPosition(latitude, longitude) {
         }
     }
     xhttp.open("GET", `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=${openweatherAPIKey}`, true)
-    xhttp.send()
+    xhttp.send() 
     
 }
 
+
+/**
+ * 
+ * @param {*} weatherData 
+ * @param {*} weatherImage 
+ */
 function showWeatherAtPosition(weatherData, weatherImage) {
     let date = new Date(weatherData.current.dt*1000)
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};
     console.log(date.toLocaleDateString('en-DE', options))
 
-   
+    currentWeather.style.display = "block";
     document.getElementById("date").innerHTML = "Date: " + date.toLocaleDateString('en-DE', options);
-    document.getElementById("currentTemp").innerHTML = "Temperature: " + weatherData.current.temp + " degree celcius";
-    document.getElementById("feelsLikeTemp").innerHTML = "Feels-like: " + weatherData.current.feels_like + " degree celsius";
-    document.getElementById("currentWind").innerHTML = "Windspeed and direction: " + weatherData.current.wind_speed + " m/s, " + weatherData.current.wind_deg + " degrees";
+    document.getElementById("currentTemp").innerHTML = "Temperature: " + Math.round(weatherData.current.temp) + " °C";
+    document.getElementById("feelsLikeTemp").innerHTML = "Feels-like: " + Math.round(weatherData.current.feels_like) + " °C";
+    document.getElementById("currentWind").innerHTML = "Windspeed and direction: " + weatherData.current.wind_speed + " m/s, " + weatherData.current.wind_deg + " °";
     document.getElementById("currentHumidity").innerHTML = "Humidity: " + weatherData.current.humidity + " %";
     document.getElementById("currentClouds").innerHTML = "Cloudiness: " + weatherData.current.clouds + " %";
-    document.body.appendChild(weatherImage);
+    document.getElementById("weatherImage").src = weatherImage.src;
 
 }
 
-function error(error) {
+
+/**
+ * 
+ * @param {*} latitude 
+ * @param {*} longitude 
+ */
+function getCity(latitude, longitude) {
+    var xhr = new XMLHttpRequest()
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let res = JSON.parse(this.responseText);
+            console.log(res);
+            
+
+            document.getElementById("cityName").innerHTML = res.features[2].place_name;
+        }
+    }
+    xhr.open("GET", `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${mapboxAPIToken}`, true)
+    xhr.send()
+}
+
+
+
+/**
+ * 
+ */
+buttonPosition.addEventListener('click', function(){
+    navigator.geolocation.getCurrentPosition(success, error);  
+})
+
+
+/**
+ * 
+ * @param {*} pos 
+ */
+function success(pos) {
+    console.log(pos);
+    var position = pos.coords;
+
+    currentPosition.style.display = "block";
+    document.getElementById("accuracy").innerHTML = `Accuracy:  ${position.accuracy}`;
+
+    getCity(position.latitude, position.longitude);
+    loadWeatherAtPosition(position.latitude, position.longitude);
+}
+
+
+function error() {
     alert("Error: User denied access to position")
 }
-
-// navigator.geolocation.getCurrentPosition(success);
-
-
-let buttonPosition = document.getElementById("buttonPosition")
-
-buttonPosition.addEventListener('click', function(){
-    let position = navigator.geolocation.getCurrentPosition(success, error);  
-})
