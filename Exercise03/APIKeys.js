d23ae41d4f2d1afd224a8d977e89dd358ab24bb4
fab-scm <fabian.schumacher@uni-muenster.de>
2021-05-31 11:23:27 +0200

    Remove ignored files

---
+++ Exercise03/APIKeys.js
@@ -0,0 +1,3 @@
+// API Keys
+const openweatherAPIKey = "97bc8bb7723c267f11e85434445965a9";
+const mapboxAPIToken = "pk.eyJ1IjoidGhhbGlzZ29sZCIsImEiOiJja29paDQ0Mm0wa3ZwMnBzMWFqanBmdmJjIn0.Rr-_s_YVsWA4TEG_xKKYdw"
