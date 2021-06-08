ed0d86017f532d1e26bf6287001927667b98dec1
fab-scm <fabian.schumacher@uni-muenster.de>
2021-06-08 15:07:27 +0200

    express server initialisation

---
+++ Exercise04_05_06/expressServer.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => 
+{
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.use(express.static('public'))
+
+
+app.listen(port, () => 
+{
+  console.log(`App listening at http://localhost:${port}`)
+})
