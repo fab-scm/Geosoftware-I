22a6937c9fc62f6a8cf3a6e810972d3862c752cb
thalisgold <thalis97@hotmail.de>
2021-09-08 15:55:51 +0200

    Creating HTML table of sights in a tour

--- Project_Exercise/public/javascripts/home.js
+++ Project_Exercise/public/javascripts/home.js
@@ -1,23 +1,21 @@
 "use strict"
-
+let table = document.getElementById("table");
 var checkbox = document.querySelector("input[type=checkbox]");
 
 $('input[type=checkbox]').change(function() {
-    //$(this).siblings('input[type="checkbox"]').prop('checked', false);
     $('input.chb').not(this).prop('checked', false);
     if (this.checked) {
-        console.log(this.id);
         let tour = tours.find(x => x._id === this.id);
-        console.log(tour);
         addSightsFromDB(tour.items);
+        // Refers to the table body from the html-document and inserts the code generated in the makeTableHTML-function.
+        table.innerHTML = makeTableHTML(fillContentTable(tour));
+        table.style="display: block;";
     } else {
-        console.log("Checkbox is not checked..");
+        map.removeLayer(markers);
+        markers = new L.FeatureGroup();
+        table.style="display: none;";
     }
 });
-
-function isEqual(tour, id) {
-    return tour._id === id;
-}
 
 // variables that store the delete-Button HTML-object
 var deleteButton = document.getElementById('deleteTourButton');
@@ -54,7 +52,8 @@ deleteButton.addEventListener('click', function(){
 
 /**
   * The function iterates through all HTML-objects from type input:checkbox
-  * and puts all ids of the checked boxes into one array which is stored as an js object.
+  * and puts all ids of the checked boxes into one array which is stored as an js object. Only one box can be checked at the same time, so 
+  * the length of the array is <=1.
   * 
   * @returns {object} the object that contains an array with the ids of all the checked boxes in the HTML-document
   */
@@ -71,3 +70,47 @@ deleteButton.addEventListener('click', function(){
     });
     return obj;
  }
+
+ /**
+ * The function creates HTML-Code to generate a table out of a two-dimensional array
+ * that can be pasted into an HTML document afterwards.
+ * 
+ * @param {2D array} myArray 
+ * @returns -  a String of the generated HTML-Code
+ */
+ function makeTableHTML(myArray) {
+
+    var result = "<table border=1><tr><th>#</th><th>Name</th><th>URL</th>";
+    for(var i=0; i<myArray.length; i++) {
+        result += "<tr>";
+        for(var j=0; j<myArray[i].length; j++){
+            result += "<td>"+myArray[i][j]+"</td>";
+        }
+        result += "</tr>";
+    }
+    result += "</table>";
+
+    return result;
+}
+
+/**
+  * The function fills the array contentTable with the right information:
+  * first column: index of the sight
+  * second column: name of the sight
+  * third column: url of the sight
+  *  
+  * @param {geojson} tour - geojson object of a tour with its name and sights...
+  */
+ function fillContentTable(tour){
+     
+    let tableData = [];     // Array that gets filled with the content for the table shown on the HTML page
+    for (let i = 0; i < tour.items.length; i++) {
+        var tableColumn = [];
+        tableColumn[0] = i + 1;
+        tableColumn[1] = tour.items[i].features[0].properties.Name;
+        tableColumn[2] = tour.items[i].features[0].properties.URL;;
+        
+        tableData.push(tableColumn);
+    }
+    return tableData;
+}
